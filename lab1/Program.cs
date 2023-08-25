08bc113f125022589499fb9b7e7f54e8327fb554
reyhan1857 <reyhanaas@code.edu.az>
2023-08-25 12:19:32 +0400

    lab1

---
+++ lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿//int num = 22;
+
+//if ( num % 3==0 && num % 7==0 )
+//{
+//    Console.WriteLine("3-e ve 7-e bolunsun");
+//}
+//else
+//{
+//    Console.WriteLine("3-e ve bolunmesin")
+
+int day = 5;
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Bazar ertesi");
+        break;
+    case 2:
+        Console.WriteLine("cersenbe axsami");
+        break;
+    case 3:
+        Console.WriteLine("cersenbe");
+        break;
+    case 4:
+        Console.WriteLine("cume axwami");
+        break;
+    case 5:
+        Console.WriteLine("cume");
+        break;
+}
+          
+          
+        
+
